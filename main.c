b658b27fe5e1f869cb5b7f015568bc1678512327
RahmanKarimli <you@example.com>
2024-08-21 12:07:05 +0400

    update main file

--- main.c
+++ main.c
@@ -1,60 +1,104 @@
 #include "main.h"
+
+/**
+ * handle_commands_array - func for handling array of commands
+ * @commands_array: array of commands
+ */
+void handle_commands_arr(char **commands_arr)
+{
+	char *command;
+	int i = 0;
+
+	if (strcmp(commands_arr[i], "env") == 0)
+	{
+		print_environ();
+	}
+	else if (strcmp(comands_arr[i], "exit") == 0)
+	{
+		exit(0);
+	}
+	else
+	{
+		for (; commands_arr[i]; i++)
+		{
+			command = commands_arr[i];
+			if (strcmp(command, "exit") == 0 && i > 0)
+			{
+				exit(1);
+				handle_command(command);
+			}
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * split_newline - seperates by newline
+ * @commands: commands
+ * @commands_arr: array for all commands
+ *
+ * Return - void
+*/
+void split_newline(char *commands, char **commands_arr)
+{
+	char *command;
+	int i = 0;
+
+	command = strtok(commands, "\n");
+	for (; command; i++)
+	{
+		commands_arr[i] = command;
+		command = strtok(NULL, "\n");
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /**
  * main - main function
+ *
  * Return: int
  */
 int main(void)
 {
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t nread;
-    pid_t pid;
-    char *command;
-    char *argv[2];
+	char *commands[MAX_LEN];
+	char *commands_arr[MAX_LEN];
+	ssize_t nread;
 
-    while (1)
-    {
-        printf("#cisfun$ ");
-        nread = getline(&line, &len, stdin);
+	while (1)
+	{
+		nread = read(STDIN_FILENO, commands, MAX_LEN);
+		exit(EXIT_FAILURE);
+		else if (nread == 0)
+		{
+			break;
+		}
+		commands[nread] = '\0';
 
-        if(nread == -1)
-        {
-                printf("\n");
-                break;
-        }
-
-        line[nread - 1] = '\0';
-
-        command  = strtok(line, " ");
-
-        if (command == NULL)
-        {
-                continue;
-        }
-
-        argv[0] = command;
-        argv[1] = NULL;
-
-        pid = fork();
-        if (pid == -1)
-        {
-                perror("fork");
-                continue;
-        }
-        else if (pid == 0)
-        {
-                if (execve(command, argv, environ) == -1)
-                {
-                        perror(command);
-                        exit(EXIT_FAILURE);
-                }
-        }
-        else
-        {
-                wait(NULL);
-        }
-    }
-
-        free(line);
-    return (0);
+		split_newline(commands, commands_arr);
+		handle_commands_array(comands_arr);
+	}
+	return (0);
 }
